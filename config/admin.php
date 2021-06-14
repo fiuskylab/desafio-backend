59784e40f6f4bc0aa413646c15b93ab69845a09c
Rafael Breno <rafael_breno@outlook.com>
2021-06-14 18:57:48 -0300

    feat: Add admin env variables into config file

---
+++ config/admin.php
@@ -0,0 +1,8 @@
+<?php 
+
+return [
+    'name' => env("ADMIN_NAME", "Admin Istrator"),
+    'email' => env("ADMIN_EMAIL", "admin@admin.com"),
+    'password' => env("ADMIN_PASSWORD", "admin"),
+    'role' => env("ADMIN_ROLE", "admin"),
+];
